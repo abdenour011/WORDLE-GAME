2fcc8222d6c8495086bc8df7f6ca0c648e75f42d
abdenour011 <wassimakacem@gmail.com>
2025-12-20 14:52:07 +0100

    Update wordle.h

--- wordle.h
+++ wordle.h
@@ -5,28 +5,28 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <stdbool.h>
+#include <ctype.h>
 
-#define WORD_LEN 5
+
+#define WORD_LENGTH 5
 #define MAX_GUESSES 6
+#define MAX_WORDS 5000
 #define DICT_FILE "words.txt"
 
 
-#define GRAY 0
-#define YELLOW 1
-#define GREEN 2
+#define GREEN   "\033[1;32m"
+#define YELLOW  "\033[1;33m"
+#define GRAY    "\033[1;30m"
+#define RESET   "\033[0m"
+#define BOLD    "\033[1m"
+#define RED     "\033[1;31m"
+#define CYAN    "\033[1;36m"  // <--- This was missing in your old file
 
-typedef struct {
-    char **words;
-    int count;
-} WordList;
 
-
-WordList load_dictionary(const char *filename);
-void free_dictionary(WordList *wl);
-void get_feedback(const char *target, const char *guess, int *feedback);
-void print_feedback(const char *guess, const int *feedback);
-bool is_valid_guess(WordList *wl, const char *guess);
-void solve_wordle(WordList *full_list, const char *target, bool debug_mode);
+void load_dictionary();
+int is_valid_word(const char *word);
+void to_upper_str(char *str);
+void play_game();   
+void solve_game();  
 
 #endif
