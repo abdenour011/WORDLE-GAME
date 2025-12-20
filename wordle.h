c5b97814499b8fa56fc075ab5a3ccc12a41276e6
abdenour011 <wassimakacem@gmail.com>
2025-12-20 14:52:20 +0100

    Update wordle.h

--- wordle.h
+++ wordle.h
@@ -7,12 +7,10 @@
 #include <time.h>
 #include <ctype.h>
 
-
 #define WORD_LENGTH 5
 #define MAX_GUESSES 6
 #define MAX_WORDS 5000
 #define DICT_FILE "words.txt"
-
 
 #define GREEN   "\033[1;32m"
 #define YELLOW  "\033[1;33m"
@@ -21,7 +19,6 @@
 #define BOLD    "\033[1m"
 #define RED     "\033[1;31m"
 #define CYAN    "\033[1;36m"  // <--- This was missing in your old file
-
 
 void load_dictionary();
 int is_valid_word(const char *word);
